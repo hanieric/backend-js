e3dae2f8236fd3d7955aa4771cad927c719b644a
Samuel Onasis <samuelonasis23@gmail.com>
2025-06-13 11:34:17 +0700

    refactor: uses dotenv

--- index.js
+++ index.js
@@ -1,52 +1,58 @@
-const express = require('express');
-const cors = require('cors')
-const session = require('express-session');
-const app = express()
-const routerfront = require('./routers/front.js')
-const routermain = require('./routers/main.js')
+const express = require("express");
+const cors = require("cors");
+const session = require("express-session");
+const routerfront = require("./routers/front.js");
+const routermain = require("./routers/main.js");
+const dotenv = require("dotenv");
+dotenv.config();
 
-app.use(session({
-  secret: 'naruto0105', 
-  resave: false,
-  saveUninitialized: true
-}));
-app.use(cors())
-app.use(express.text())
-app.use(express.json())
+const app = express();
+
+app.use(
+  session({
+    secret: dotenv.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+  })
+);
+
+app.use(cors());
+app.use(express.text());
+app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.use(routerfront)
-app.use(routermain)
+app.use(routerfront);
+app.use(routermain);
 
 const { createServer } = require("http");
 const httpServer = createServer(app);
 const io = require("socket.io")(httpServer, {
   cors: {
-    origin: "http://localhost:5173",
+    origin: dotenv.CORS_ORIGIN,
     methods: ["GET", "POST"],
     allowedHeaders: ["my-custom-header"],
-    credentials: true
-  }
+    credentials: true,
+  },
 });
 
-io.on('connection', (socket) => {
-  console.log('Client connected');
+io.on("connection", (socket) => {
+  console.log("Client connected");
 
-  socket.on('message', (data) => {
-      console.log('Message from client:', data);
-      io.emit('message', data);
+  socket.on("message", (data) => {
+    console.log("Message from client:", data);
+    io.emit("message", data);
   });
 
-  socket.on('disconnect', () => {
-      console.log('Client disconnected');
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 });
 
 httpServer.listen(3001, () => {
-  console.log(`SERVER REALTIME DIJALANKAN DI PORT 3001`)
-})
+  console.log(`SERVER REALTIME DIJALANKAN DI PORT 3001`);
+});
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`SERVER DIJALANKAN DI PORT ${PORT}`)
-})
+  console.log(`SERVER DIJALANKAN DI PORT ${PORT}`);
+});
--- routers/front.js
+++ routers/front.js
@@ -1,90 +1,92 @@
-const express = require('express')
-const mysql = require('mysql2');
-const router = express.Router()
-const jwt = require('jsonwebtoken');
-const bcrypt = require('bcrypt');
-const connection = mysql.createConnection({
-    host: 'sql12.freesqldatabase.com',
-    user: 'sql12677382',
-    password: 'IpqW6Appgu',
-    database: 'sql12677382',
+const express = require("express");
+const mysql = require("mysql2");
+const router = express.Router();
+const jwt = require("jsonwebtoken");
+const bcrypt = require("bcrypt");
+const dotenv = require("dotenv");
+dotenv.config();
+
+const connection = require("../lib/mysql_connection.js");
+
+router.get("/", (req, res) => {
+  res.send("WELCOME TO MY FINANCIAL WEB");
 });
 
-router.get('/',(req,res)=>{
-    res.send('WELCOME TO MY FINANCIAL WEB')
-})
+router.post("/register", (req, res) => {
+  const user = req.body;
 
-router.post('/register',(req,res)=>{
-    const user = req.body
+  if (!user.username || !user.password) {
+    res.status(400).send("wajib menyertakan username dan password");
+    return;
+  }
 
-    if (!user.username || !user.password) {
-        res.status(400).send('wajib menyertakan username dan password')
-        return;
+  bcrypt.hash(user.password, 10, (err, result) => {
+    if (err) {
+      res.status(500).send("masalah dengan server");
+      return;
     }
 
-    bcrypt.hash(user.password, 10, (err, result) => {
-        if (err) {
-        res.status(500).send('masalah dengan server')
+    const q = `INSERT INTO user (username, password) VALUES ('${user.username}', '${result}')`;
+    connection.query(q, (err, result) => {
+      if (err) {
+        res.status(500).send("Ada masalah dengan hubungan ke server");
+        console.error(err);
         return;
-        }
-        
-        const q = `INSERT INTO user (username, password) VALUES ('${user.username}', '${result}')`
-        connection.query(q, (err, result) => {
-            if (err) {
-                res.status(500).send('Ada masalah dengan hubungan ke server')
-                console.error(err)
-                return;
-            }
-            
-            res.status(201).send('Registrasi User berhasil')
-        })
-    })
-})
+      }
 
-router.post('/login',(req,res)=>{
+      res.status(201).send("Registrasi User berhasil");
+    });
+  });
+});
+
+router.post("/login", (req, res) => {
   const user = req.body;
-  const q = `SELECT * FROM user WHERE username = '${user.username}'`
+  const q = `SELECT * FROM user WHERE username = '${user.username}'`;
   connection.query(q, (err, result) => {
     if (err) {
-        res.status(500).send('Ada masalah dengan hubungan ke server')
-        console.error(err)
-        return;
-    }
-    
-    if(result.length > 0){
-    bcrypt.compare(user.password, result[0].password, (err, hasilCompare) => {
-      if (err) {
-        res.status(500).send('ada masalah pada server')
-        return
-      }
-  
-      if (hasilCompare) {
-        req.user = result[0]
-        const token = jwt.sign({ userId: result[0].id}, 'naruto0105', { expiresIn: '6h' });
-        req.session.token = token;
-        res.status(200).send({token:token,user : result[0]});
-        return
-      } else {
-        res.status(401).send('gagal login');
-        return
-      }
-    });
+      res.status(500).send("Ada masalah dengan hubungan ke server");
+      console.error(err);
+      return;
     }
 
-    else{
-      res.status(404).send('username tidak ditemukan')
+    if (result.length > 0) {
+      bcrypt.compare(user.password, result[0].password, (err, hasilCompare) => {
+        if (err) {
+          res.status(500).send("ada masalah pada server");
+          return;
+        }
+
+        if (hasilCompare) {
+          req.user = result[0];
+          const token = jwt.sign(
+            { userId: result[0].id },
+            dotenv.SESSION_SECRET,
+            {
+              expiresIn: "6h",
+            }
+          );
+          req.session.token = token;
+          res.status(200).send({ token: token, user: result[0] });
+          return;
+        } else {
+          res.status(401).send("gagal login");
+          return;
+        }
+      });
+    } else {
+      res.status(404).send("username tidak ditemukan");
     }
   });
 });
 
-router.get('/logout', (req, res) => {
+router.get("/logout", (req, res) => {
   req.session.destroy((err) => {
     if (err) {
-      res.send('Error saat logout');
-      return
+      res.send("Error saat logout");
+      return;
     }
-    res.status(200).send('Berhasil logout')
+    res.status(200).send("Berhasil logout");
   });
 });
 
-module.exports = router
+module.exports = router;
