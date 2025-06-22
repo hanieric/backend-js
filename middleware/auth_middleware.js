f9d02e5f86f8690d7c1a63c76aecc7311dc6a414
Samuel Onasis <samuelonasis23@gmail.com>
2025-06-22 23:22:29 +0700

    feat: check user eixstance

--- middleware/auth_middleware.js
+++ middleware/auth_middleware.js
@@ -1,6 +1,7 @@
 import jwt from "jsonwebtoken";
+import mysql_connection from "../lib/mysql_connection.js";
 
-const authMiddleware = (req, res, next) => {
+const authMiddleware = async (req, res, next) => {
   const authHeader = req.headers["authorization"];
   if (!authHeader) {
     return res.status(401).json({ message: "Authorization header missing" });
@@ -14,7 +15,16 @@ const authMiddleware = (req, res, next) => {
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
 
+    const user = await mysql_connection.SELECT("user", {
+      where: { id: decoded.userId },
+    });
+
+    if (user.length === 0) {
+      return res.status(401).json({ message: "User is deleted." });
+    }
+
     req.userId = decoded.userId;
+
     next();
   } catch (err) {
     return res.status(403).json({ message: "Invalid or expired token" });
